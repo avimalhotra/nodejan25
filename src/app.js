90a35dbbe085222e68d78abacea794eff82f74fe
Avinash <avimalhotra505@gmail.com>
2025-02-15 11:01:33 +0530

    node js http

--- src/app.js
+++ src/app.js
@@ -1,80 +1,45 @@
-const fs=require('fs');
+const http=require("http");
+const ip='127.0.0.1';
+const port=process.env.PORT || 3000;
+const fs=require("fs");
 const path=require("path");
 
-// const res=fs.readFileSync("src/data.txt",{encoding:'utf-8'});
-// const res=fs.readFileSync(path.resolve("src/data.txt"),{encoding:'utf-8'});
 
+const server=http.createServer((req,res)=>{  
+    // res.write(req.url);
+    // res.write(req.method);
+    // res.write(req.headers.host);
+    // res.write("Hello World");
+    // res.statusCode=200;
+    // res.setHeader('Content-Type','text/html');
+    // res.writeHead(200,{'content-type':'text/html'});
+    // res.write("<h1>Hello World</h1>");
+    // res.write("<p>");
+    // res.write(new Date().toLocaleString());
+    // res.write("</p>");
+    // res.end();
 
-// fs.readFile(path.resolve("src/data.txt"),{encoding:'utf-8'},(err,res)=>{
-//   // if(err){ console.warn(err)}
-//   // else{ console.log(res) }
-  
-//   fs.stat(path.resolve("src/data.txt"),(err,res)=>{
-//     if(err){ console.warn(err)}
-//     else{ 
-//       console.log(res.size);
-//       console.log(res.isDirectory() );
-//       console.log(res.isFile() );
-//      }
-//   });
+    if(req.url=="/" && req.method=='GET'){
+      fs.readFile(path.resolve('src/index.html'),(err,data)=>{
+        if(err){
+          res.writeHead(404,{'content-type':'text/html'});
+          res.end("File Not Found");
+        }
+        else{
+          res.writeHead(200,{'content-type':'text/html'});
+          res.end(data);
+        }
+      })
+    }
+    else if(req.url=="/app" && req.method=='GET'){
+      res.writeHead(200,{'content-type':'text/html'});
+      res.end("App module");
+    }
+    else{
+      res.end("unknown error");
+    }
+});
 
-// });
-
-// fs.writeFile(path.resolve("src/data.txt"),"hello world",{encoding:'utf-8'},(err)=>console.warn(err));
-// fs.appendFile(path.resolve("src/data.txt"),"hello world\n",{encoding:'utf-8'},(err)=>console.warn(err));
-
-
-// fs.unlink(path.resolve("src/data.txt"),err=>console.warn(err));
-
-// try{
-//   fs.unlink(path.resolve("src/data.txt"),err=>console.warn(err));
-// }
-// catch(err){
-//   console.warn(err);
-// }
-
-// console.log( "done" );
-
-
-/* events  */
-
-/* fs.ReadStream(path.resolve("src/data.txt")).on("open",()=>{
-  console.log("open");
-}); */
-
-const event=require("events").EventEmitter;
-const emitter=new event();
-module.exports=emitter;
-
-// emitter.on("done",(user,x)=>{
-//   console.log(`event done ${user}`);
-//   // x.done=false;
-// });
-// emitter.on("done",(user,x)=>{
-//   // console.log(`again event done ${user}`);
-//   if(x.done){ console.log(`again event done ${user}`) }
-// });
-
-// emitter.once("done",(user,x)=>{
-//     console.log(`event done ${user}`);
-// });
-
-// function callOnce(){
-//   console.log("call onced");
-//   emitter.removeListener("done",callOnce);     // remove event
-// }
-
-// emitter.on("done",callOnce);
-
-
-// emitter.emit("done"," by avinash",{done:true});
-// emitter.emit("done"," by isha",{done:true});
-
-
-
-require("./registration");
-require("./account");
-
-
-emitter.emit('register',new Date());
-emitter.emit('account',"sbi");
+server.listen(port,ip,()=>{
+  console.log(`App running at http://${ip}:${port}`);
+});
--- src/old.js
+++ src/old.js
@@ -98,3 +98,85 @@ async function getNetworkUploadSpeed() {
   const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
   console.log("Upload",speed);
 }
+
+
+const fs=require('fs');
+const path=require("path");
+
+// const res=fs.readFileSync("src/data.txt",{encoding:'utf-8'});
+// const res=fs.readFileSync(path.resolve("src/data.txt"),{encoding:'utf-8'});
+
+
+// fs.readFile(path.resolve("src/data.txt"),{encoding:'utf-8'},(err,res)=>{
+//   // if(err){ console.warn(err)}
+//   // else{ console.log(res) }
+  
+//   fs.stat(path.resolve("src/data.txt"),(err,res)=>{
+//     if(err){ console.warn(err)}
+//     else{ 
+//       console.log(res.size);
+//       console.log(res.isDirectory() );
+//       console.log(res.isFile() );
+//      }
+//   });
+
+// });
+
+// fs.writeFile(path.resolve("src/data.txt"),"hello world",{encoding:'utf-8'},(err)=>console.warn(err));
+// fs.appendFile(path.resolve("src/data.txt"),"hello world\n",{encoding:'utf-8'},(err)=>console.warn(err));
+
+
+// fs.unlink(path.resolve("src/data.txt"),err=>console.warn(err));
+
+// try{
+//   fs.unlink(path.resolve("src/data.txt"),err=>console.warn(err));
+// }
+// catch(err){
+//   console.warn(err);
+// }
+
+// console.log( "done" );
+
+
+/* events  */
+
+/* fs.ReadStream(path.resolve("src/data.txt")).on("open",()=>{
+  console.log("open");
+}); */
+
+const event=require("events").EventEmitter;
+const emitter=new event();
+module.exports=emitter;
+
+// emitter.on("done",(user,x)=>{
+//   console.log(`event done ${user}`);
+//   // x.done=false;
+// });
+// emitter.on("done",(user,x)=>{
+//   // console.log(`again event done ${user}`);
+//   if(x.done){ console.log(`again event done ${user}`) }
+// });
+
+// emitter.once("done",(user,x)=>{
+//     console.log(`event done ${user}`);
+// });
+
+// function callOnce(){
+//   console.log("call onced");
+//   emitter.removeListener("done",callOnce);     // remove event
+// }
+
+// emitter.on("done",callOnce);
+
+
+// emitter.emit("done"," by avinash",{done:true});
+// emitter.emit("done"," by isha",{done:true});
+
+
+
+require("./registration");
+require("./account");
+
+
+emitter.emit('register',new Date());
+emitter.emit('account',"sbi");
