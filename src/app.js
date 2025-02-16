24489729da98cb3399d53440c45138241bc22eea
Avinash <avimalhotra505@gmail.com>
2025-02-16 11:02:34 +0530

    express js

--- src/app.js
+++ src/app.js
@@ -1,45 +1,82 @@
-const http=require("http");
-const ip='127.0.0.1';
-const port=process.env.PORT || 3000;
-const fs=require("fs");
+const express=require("express");
+const app=express();
 const path=require("path");
+require("dotenv").config();
+const ip="127.0.0.1";
+const port=process.env.PORT || 3000;
 
+/* app.use((req,res)=>{
+  res.status(200);
+  res.setHeader('Content-Type','text/html');
+  res.end(`<h1>Hello Express, ${new Date().toLocaleString()}</h1>`);
+}); */
 
-const server=http.createServer((req,res)=>{  
-    // res.write(req.url);
-    // res.write(req.method);
-    // res.write(req.headers.host);
-    // res.write("Hello World");
-    // res.statusCode=200;
-    // res.setHeader('Content-Type','text/html');
-    // res.writeHead(200,{'content-type':'text/html'});
-    // res.write("<h1>Hello World</h1>");
-    // res.write("<p>");
-    // res.write(new Date().toLocaleString());
-    // res.write("</p>");
-    // res.end();
+app.use(express.static(path.resolve('src/public')));
 
-    if(req.url=="/" && req.method=='GET'){
-      fs.readFile(path.resolve('src/index.html'),(err,data)=>{
-        if(err){
-          res.writeHead(404,{'content-type':'text/html'});
-          res.end("File Not Found");
-        }
-        else{
-          res.writeHead(200,{'content-type':'text/html'});
-          res.end(data);
-        }
-      })
-    }
-    else if(req.url=="/app" && req.method=='GET'){
-      res.writeHead(200,{'content-type':'text/html'});
-      res.end("App module");
-    }
-    else{
-      res.end("unknown error");
-    }
+app.use((req,res,next)=>{
+  console.log(`App starts at ${new Date().toLocaleString()}`);
+  next();
 });
 
-server.listen(port,ip,()=>{
-  console.log(`App running at http://${ip}:${port}`);
+app.get('/',(req,res)=>{
+  res.setHeader('Content-Type','text/html');
+  res.status(200).send(`<h1>Hello Express, ${new Date().toLocaleString()}</h1>`);
+});
+app.get('/login',(req,res)=>{
+  res.setHeader('Content-Type','text/html');
+  res.status(200).send(`<h1>Login</h1>`);
+});
+app.get('/search',(req,res)=>{
+  res.setHeader('Content-Type','text/html');
+  res.status(200).send(req.query);
+});
+// app.get('/contact.php',(req,res)=>{
+//   res.setHeader('Content-Type','text/html');
+//   res.redirect("/contact");
+// });
+
+app.get('/download',(req,res)=>{
+  res.status(200).download(path.resolve("src/public/img/bat.svg"),(err)=>{
+    res.status(404).send("file not found");
+  });
+})
+app.get('/contact',(req,res)=>{
+  res.setHeader('Content-Type','text/html');
+  res.status(200).send("Contact page");
+})
+
+app.get('/product',(req,res)=>{
+  res.setHeader('Content-Type','text/html');
+  res.status(200).send("Product page");
+});
+app.get('/product/:brand',(req,res)=>{
+  res.setHeader('Content-Type','text/html');
+  res.status(200).send(req.params);
+});
+app.get('/product/:brand/:car',(req,res)=>{
+  res.setHeader('Content-Type','text/html');
+  res.status(200).send(req.params);
+});
+
+
+app.get('/api',(req,res)=>{
+  res.setHeader('Content-Type','text/html');
+  res.status(200).json({name:"avi",id:22});
+});
+
+
+app.post("/login",(req,res)=>{
+  res.status(200).send("post")
+});
+
+
+
+/* wild card handler */
+app.get("/**",(req,res)=>{
+  res.status(404).send(`<h1>Page not found</h1><p>Go to <a href="/">Homepage</a></p>`);
+});
+
+
+app.listen(port,ip,()=>{
+  console.log(`app running at http://${ip}:${port}`)
 });
--- src/old.js
+++ src/old.js
@@ -144,9 +144,9 @@ const path=require("path");
   console.log("open");
 }); */
 
-const event=require("events").EventEmitter;
-const emitter=new event();
-module.exports=emitter;
+// const event=require("events").EventEmitter;
+// const emitter=new event();
+// module.exports=emitter;
 
 // emitter.on("done",(user,x)=>{
 //   console.log(`event done ${user}`);
@@ -173,10 +173,56 @@ module.exports=emitter;
 // emitter.emit("done"," by isha",{done:true});
 
 
+// require("./registration");
+// require("./account");
 
-require("./registration");
-require("./account");
+
+// emitter.emit('register',new Date());
+// emitter.emit('account',"sbi");
 
 
-emitter.emit('register',new Date());
-emitter.emit('account',"sbi");
+// const http=require("http");
+// const ip='127.0.0.1';
+// const port=process.env.PORT || 3000;
+// const fs=require("fs");
+// const path=require("path");
+
+
+// const server=http.createServer((req,res)=>{  
+    // res.write(req.url);
+    // res.write(req.method);
+    // res.write(req.headers.host);
+    // res.write("Hello World");
+    // res.statusCode=200;
+    // res.setHeader('Content-Type','text/html');
+    // res.writeHead(200,{'content-type':'text/html'});
+    // res.write("<h1>Hello World</h1>");
+    // res.write("<p>");
+    // res.write(new Date().toLocaleString());
+    // res.write("</p>");
+    // res.end();
+
+//     if(req.url=="/" && req.method=='GET'){
+//       fs.readFile(path.resolve('src/index.html'),(err,data)=>{
+//         if(err){
+//           res.writeHead(404,{'content-type':'text/html'});
+//           res.end("File Not Found");
+//         }
+//         else{
+//           res.writeHead(200,{'content-type':'text/html'});
+//           res.end(data);
+//         }
+//       })
+//     }
+//     else if(req.url=="/app" && req.method=='GET'){
+//       res.writeHead(200,{'content-type':'text/html'});
+//       res.end("App module");
+//     }
+//     else{
+//       res.end("unknown error");
+//     }
+// });
+
+// server.listen(port,ip,()=>{
+//   console.log(`App running at http://${ip}:${port}`);
+// });
